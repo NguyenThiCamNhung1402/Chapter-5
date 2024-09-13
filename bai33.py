f4eb641ab7b06c0d8d41078a58fd09083bcf80c4
NguyenThiCamNhung1402 <camnhung140261@gmail.com>
2024-09-14 00:41:02 +0700

    bai33.py

--- bai33.py
+++ bai33.py
@@ -5,17 +5,16 @@ Created on Fri Sep 13 21:51:06 2024
 @author: Nguyễn Thị Cẩm Nhung -23712471 
 """
 n = int(input("Nhập số nguyên dương: "))
-if n <= 0:
-    print("Số nhập vào không phải là số nguyên dương.")
+while  n <= 0:
+    n = int(input("Nhập lại số nguyên dương: "))
+x   = False
+i = 1
+while i * i <= n:
+    if i * i == n:
+        x   = True
+        break
+    i += 1
+if x :
+    print(f"{n} là số chính phương.")
 else:
-    x   = False
-    i = 1
-    while i * i <= n:
-        if i * i == n:
-            x   = True
-            break
-        i += 1
-    if x :
-        print(f"{n} là số chính phương.")
-    else:
-        print(f"{n} không phải là số chính phương.")
+    print(f"{n} không phải là số chính phương.")
