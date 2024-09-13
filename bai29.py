f1ee79f6a94663212eae8639f436e517cc3c5d4d
NguyenThiCamNhung1402 <camnhung140261@gmail.com>
2024-09-14 00:35:15 +0700

    bai29.py

--- bai29.py
+++ bai29.py
@@ -4,8 +4,12 @@ Created on Thu Sep 12 22:24:51 2024
 
 @author: Nguyễn Thị Cẩm Nhung - 23712471  
 """
-N = input("Nhập số nguyên dương N: ")
-x = 0
-for i in N:
-    x += int(i)
-print("Tổng các chữ số là:", x )
+N = int(input("Nhập số nguyên dương N: "))
+while N <= 0:
+    N = int(input("Không hợp lệ. Nhập lại số nguyên dương: "))
+tong = 0
+so_du = N
+while so_du > 0:
+    tong += so_du % 10  
+    so_du //= 10       
+print("Tổng các chữ số của", N, "là:", tong)
