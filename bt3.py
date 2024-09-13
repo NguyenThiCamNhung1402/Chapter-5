6dd19317ff8a073ad00fc3acd98d3002ce36e152
NguyenThiCamNhung1402 <camnhung140261@gmail.com>
2024-09-14 01:14:10 +0700

    Add files via upload

---
+++ bt3.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 12 20:34:15 2024
+
+@author: Nguyễn Thị Cẩm Nhung -23712471
+"""
+import random
+n = random.randint(20, 30)
+ds1  = [random.random() for i in range(n)]
+print(ds1 )
+ds2  = [random.uniform(18, 99)**2 for x  in range(10)]
+print(ds2 )
