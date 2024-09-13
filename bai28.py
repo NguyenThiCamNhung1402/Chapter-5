98b1812a9fff4df2dff61e4799050b7e56312350
NguyenThiCamNhung1402 <camnhung140261@gmail.com>
2024-09-14 00:28:23 +0700

    bai28.py

--- bai28.py
+++ bai28.py
@@ -4,16 +4,11 @@ Created on Thu Sep 12 21:24:36 2024
 
 @author: Nguyễn Thị Cẩm Nhung -23712471 
 """
-while True:
-    N = input("Nhập số nguyên dương N: ")
-    if N.isdigit():
-        N = int(N)
-        if N > 0:
-            break
-    else:
-        print("Số nhập vào không hợp lệ! Vui lòng nhập lại.")
+N = int(input("Nhập số nguyên dương N: "))
+while N<= 0:
+    N = int(input("Không hợp lệ. Nhập lại: ")) 
 print("Các ước số của", N, "là:")
 for i in range(1, N + 1):
     if N % i == 0:
-        print(i)
+        print(i) 
      
