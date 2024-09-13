2811660118685e00c63631cdd2ef8f25bd3c57d9
NguyenThiCamNhung1402 <camnhung140261@gmail.com>
2024-09-14 00:54:53 +0700

    tonghop3.py

--- tonghop3.py
+++ tonghop3.py
@@ -4,33 +4,34 @@ Created on Fri Sep 13 21:14:54 2024
 
 @author: Nguyễn Thị Cẩm Nhung - 23712471 
 """
-user_id = input("Nhập ID user: ")
-password = input("Nhập mật khẩu: ")
-user_valid = True
-if len(user_id) < 6 or len(user_id) > 24:
-    user_valid = False
+id_nguoi_dung = input("Nhập ID user: ")
+mat_khau = input("Nhập mật khẩu: ")
+id_hop_le = True
+if len(id_nguoi_dung) < 6 or len(id_nguoi_dung) > 24:
+    id_hop_le = False
 else:
-    for c in user_id:
-        if c in "!@#$%^&*()-=+ ":
-            user_valid = False
+    for ky_tu in id_nguoi_dung:
+        if ky_tu in "!@#$%^&*()-=+ ":
+            id_hop_le = False
             break
-password_valid = True
-if len(password) < 6 or len(password) > 24:
-    password_valid = False
+mat_khau_hop_le = True
+if len(mat_khau) < 6 or len(mat_khau) > 24:
+    mat_khau_hop_le = False
 else:
-    has_lower = has_digit = has_upper = has_special = False
-    for c in password:
-        if c.islower():
-            has_lower = True
-        elif c.isdigit():
-            has_digit = True
-        elif c.isupper():
-            has_upper = True
-        elif c in "$#@":
-            has_special = True
-    if not (has_lower and has_digit and has_upper and has_special):
-        password_valid = False
-if user_valid and password_valid:
+    co_chu_thuong = co_so = co_chu_hoa = co_dau_bang = False
+    for ky_tu in mat_khau:
+        if ky_tu.islower():
+            co_chu_thuong = True
+        elif ky_tu.isdigit():
+            co_so = True
+        elif ky_tu.isupper():
+            co_chu_hoa = True
+        elif ky_tu in "$#@":
+            co_dau_bang = True
+    if not (co_chu_thuong and co_so and co_chu_hoa and co_dau_bang):
+        mat_khau_hop_le = False
+if id_hop_le and mat_khau_hop_le:
     print("Đăng nhập thành công.")
 else:
     print("Đăng nhập thất bại.")
+
