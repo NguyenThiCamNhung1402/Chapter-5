f9222016473b7e360ac041b06c6fe1511eea81e1
NguyenThiCamNhung1402 <camnhung140261@gmail.com>
2024-09-14 00:38:52 +0700

    bai31.py

--- bai31.py
+++ bai31.py
@@ -7,26 +7,27 @@ Created on Fri Sep 13 21:39:52 2024
 a = int(input("Nhập cạnh thứ nhất: "))
 b = int(input("Nhập cạnh thứ hai: "))
 c = int(input("Nhập cạnh thứ ba: "))
-if a <= 0 or b <= 0 or c <= 0:
-    loai = "Các cạnh phải là số nguyên dương"
-else:
-    if a > b:
+while a <= 0 or b <= 0 or c <= 0:
+    a = int(input("Nhập lại cạnh thứ nhất: "))
+    b = int(input("Nhập lại cạnh thứ hai: "))
+    c = int(input("Nhập lại cạnh thứ ba: "))
+if a > b:
+    a, b = b, a
+if b > c:
+    b, c = c, b
+if a > b:
         a, b = b, a
-    if b > c:
-        b, c = c, b
-    if a > b:
-        a, b = b, a
-    if a + b > c:
-        if a == b == c:
-            loai = "Tam giác đều"
-        elif a**2 + b**2 == c**2:
-            loai = "Tam giác vuông"
-        elif a == b or b == c or a == c:
-            loai = "Tam giác cân"
-        else:
-            loai = "Tam giác thường"
+if a + b > c:
+    if a == b == c:
+        loai = "Tam giác đều"
+    elif a**2 + b**2 == c**2:
+        loai = "Tam giác vuông"
+    elif a == b or b == c or a == c:
+        loai = "Tam giác cân"
     else:
-        loai = "Ba số không lập thành tam giác"
+        loai = "Tam giác thường"
+else:
+    loai = "Ba số không lập thành tam giác"
 print("Kết quả kiểm tra:", loai)
 
 
