08356ac450aff59b44424690cfd599fadac18f17
NguyenThiCamNhung1402 <camnhung140261@gmail.com>
2024-09-14 00:45:09 +0700

    bai36.py

--- bai36.py
+++ bai36.py
@@ -6,7 +6,7 @@ Created on Thu Sep 12 22:43:58 2024
 """
 n = int(input("Nhập số nguyên dương n: "))
 S = 0
-while n <0:
+while n <= 0:
     n= int(input ("Số vừa nhập không hợp lệ. Nhập lại:")) 
 for i in range(1, n + 1):
     S += i**2  
