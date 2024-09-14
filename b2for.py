02555551b22c8112213f04f8a9e756f9b91b45f8
NguyenThiCamNhung1402 <camnhung140261@gmail.com>
2024-09-14 13:30:43 +0700

    b2for.py

--- b2for.py
+++ b2for.py
@@ -5,8 +5,8 @@ Created on Sat Sep 14 01:00:59 2024
 @author: Nguyễn Thị Cẩm Nhung - 23712471 
 """
 for i in range(1, 10):
-    for j in range(1, 11):
-        print(f"{i} x {j} = {i * j}", end="\t")
+    for j in range(1, 10):
+        print(f"{i} x {j} = {i * j}")
     print()
 
 
