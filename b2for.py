a66997d481786f836a6e805303142924917ad5ab
NguyenThiCamNhung1402 <camnhung140261@gmail.com>
2024-09-14 13:21:49 +0700

    b2for.py

--- b2for.py
+++ b2for.py
@@ -4,10 +4,9 @@ Created on Sat Sep 14 01:00:59 2024
 
 @author: Nguyễn Thị Cẩm Nhung - 23712471 
 """
-for i in range(2, 10): 
-    print(f"Bảng cửu chương {i}:")  
+for i in range(1, 10):
     for j in range(1, 11):
-        print(f"{i} x {j} = {i * j}")  
-    print() 
+        print(f"{i} x {j} = {i * j}", end="\t")
+    print()
 
 
