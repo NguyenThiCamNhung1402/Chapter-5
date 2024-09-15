96513bdbc5fa24813bbba05dcf1a283b74f4b784
NguyenThiCamNhung1402 <camnhung140261@gmail.com>
2024-09-15 23:21:10 +0700

    b2while.py

--- b2while.py
+++ b2while.py
@@ -4,7 +4,15 @@ Created on Sat Sep 14 01:10:13 2024
 
 @author: Nguyễn Thị Cẩm Nhung - 23712471 
 """
-n= float(input("Nhập một giá trị: "))
-while n<= -89.9 or n>= 88.8:
-    n=float(input("Nhập lại:"))
-print("Thỏa mãn điều kiện")
+ktra_bien = False  
+while not ktra_bien:
+    ktr = input("Nhập một số thực trong khoảng [-89.9; 88.8]: ")
+    if ktr.replace('.', '', 1).replace('-', '', 1).isdigit() and ktr.count('.') <= 1:
+        value = float(ktr)  
+        if -89.9 <= value <= 88.8:
+            print(f"Giá trị nhập vào hợp lệ: {value}")
+            ktra_bien = True 
+        else:
+            print("Giá trị không nằm trong khoảng [-89.9; 88.8]. Vui lòng nhập lại.")
+    else:
+        print("Giá trị nhập vào không hợp lệ. Vui lòng nhập một số thực.")
