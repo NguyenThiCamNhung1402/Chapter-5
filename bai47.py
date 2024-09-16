5d4cd44016411a45ee307c32f27f037964da263b
NguyenThiCamNhung1402 <camnhung140261@gmail.com>
2024-09-16 12:16:54 +0700

    bai47.py

--- bai47.py
+++ bai47.py
@@ -4,24 +4,20 @@ Created on Fri Sep 13 23:54:42 2024
 
 @author: Nguyễn Thị Cẩm Nhung - 23712471
 """
-tong_lon_nhat = 0
-bo_nghiem_tot_nhat = [(0, 0, 0)] * 100  
-so_luong_nhiem = 0  
-for x in range(1, 980 // 2 + 1):
-    for y in range(1, 980 // 7 + 1):
-        z = (979 - 2 * x - 7 * y) / 9
-        if z.is_integer() and z > 0:
-            z = int(z)
-            tong_hien_tai = x + y + z
-            if tong_hien_tai > tong_lon_nhat:
-                tong_lon_nhat = tong_hien_tai
-                so_luong_nhiem = 1
-                bo_nghiem_tot_nhat[0] = (x, y, z)
-            elif tong_hien_tai == tong_lon_nhat:
-                bo_nghiem_tot_nhat[so_luong_nhiem] = (x, y, z)
-                so_luong_nhiem += 1
-print("Tất cả bộ nghiệm (x, y, z) với tổng x + y + z lớn nhất:")
-for i in range(so_luong_nhiem):
-    print(bo_nghiem_tot_nhat[i])
+max_t = 0
+al = None
+for x in range(1, 490):
+    for y in range(1, 141): 
+        for z in range(1, 109): 
+            if 2 * x + 7 * y + 9 * z == 979:
+                tong = x + y + z
+                if tong > max_t:
+                    max_t = tong
+                    al = (x, y, z)
+if al:
+    print(f"Bộ nghiệm có tổng lớn nhất là: x = {al[0]}, y = {al[1]}, z = {al[2]}")
+    print(f"Tổng x + y + z = {max_t}")
+else:
+    print("Không tìm thấy bộ nghiệm nào thỏa mãn.")
 
 
