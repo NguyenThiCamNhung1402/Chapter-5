0184657e8662aaf1d8ef7d5552e6fb074c019d1c
NguyenThiCamNhung1402 <camnhung140261@gmail.com>
2024-09-16 12:32:50 +0700

    bai48.py

--- bai48.py
+++ bai48.py
@@ -4,23 +4,27 @@ Created on Sat Sep 14 00:04:04 2024
 
 @author: Nguyễn Thị Cẩm Nhung - 23712471
 """
-tong_nho_nhat = float('inf')   
-bo_nghiem_toi_uu = [(0, 0, 0)] * 100  
-so_luong_nhiem = 0  
-for x in range(1, 980 // 2 + 1):
-    for y in range(1, 980 // 7 + 1):
-        z = (979 - 2 * x - 7 * y) / 9
-        if z.is_integer() and z > 0:
-            z = int(z)
-            tong_hien_tai = x + y + z
-            if tong_hien_tai < tong_nho_nhat:
-                tong_nho_nhat = tong_hien_tai
-                so_luong_nhiem = 1  
-                bo_nghiem_toi_uu[0] = (x, y, z)  
-            elif tong_hien_tai == tong_nho_nhat:
-                bo_nghiem_toi_uu[so_luong_nhiem] = (x, y, z)  
-                so_luong_nhiem += 1  
-print("Tất cả bộ nghiệm (x, y, z) với tổng x + y + z nhỏ nhất:")
-for i in range(so_luong_nhiem):
-    print(bo_nghiem_toi_uu[i])
+ds = []
+min_t = float('inf') 
+for z in range(1, 979 // 9 + 1):
+    tim = 979 - 9 * z 
+    if tim <= 0:
+        break
+    for y in range(1, tim // 7 + 1):
+        if (tim - 7 * y) > 0:  
+            x = (tim - 7 * y) / 2 
+            if (tim - 7 * y) % 2 == 0 and x > 0:  
+                x = int(x) 
+                tong = x + y + z 
+                if tong < min_t:
+                    min_t = tong
+                    ds = [(x, y, z)] 
+                elif tong == min_t:
+                    ds += [(x, y, z)]
+print("Các bộ nghiệm nguyên (x, y, z) với x + y + z nhỏ nhất:")
+if ds:  
+    for i in ds:
+        print(i)
+else:
+    print("Không có bộ nghiệm nào thỏa mãn.")
 
