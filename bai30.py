6d61192f81b88b3c2650273c5f56621f4d59c7ed
NguyenThiCamNhung1402 <camnhung140261@gmail.com>
2024-09-16 13:03:16 +0700

    bai30.py

--- bai30.py
+++ bai30.py
@@ -4,23 +4,20 @@ Created on Thu Sep 12 22:26:42 2024
 
 @author: Nguyễn Thị Cẩm Nhung - 23712471 
 """
-month = 0
-year = 0
+month = int(input("Nhập tháng (1-12): "))
 while month < 1 or month > 12:
-    month = int(input("Nhập tháng (1-12): "))
-    if month < 1 or month > 12:
-        print("Tháng không hợp lệ. Vui lòng nhập lại.")
+    month = int(input("Tháng không hợp lệ. Vui lòng nhập lại: "))
 year = int(input("Nhập năm: "))
 nhuan = False
 if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
     nhuan = True
 if month in [1, 3, 5, 7, 8, 10, 12]:
-    days = 31 
+    days = 31
 elif month in [4, 6, 9, 11]:
-    days = 30 
+    days = 30
 elif month == 2:
     if nhuan:
-        days = 29  
+        days = 29
     else:
-        days = 28  
+        days = 28
 print(f"Tháng {month} năm {year} có {days} ngày.")
