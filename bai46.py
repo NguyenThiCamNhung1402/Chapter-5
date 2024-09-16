038e1c2b919a2026d9561e253f1e771baadf0e78
NguyenThiCamNhung1402 <camnhung140261@gmail.com>
2024-09-16 12:04:43 +0700

    bai46.py

--- bai46.py
+++ bai46.py
@@ -4,13 +4,9 @@ Created on Thu Sep 12 22:33:46 2024
 
 @author: Nguyễn Thị Cẩm Nhung - 23712471 
 """
-nghiem  = []
-for x in range(1, 980):
-    for y in range(1, 980):
-        if (979 - 2 * x - 7 * y) % 9 == 0:
-            z = (979 - 2 * x - 7 * y) // 9
-            if z > 0:
-                nghiem += [(x, y, z)]
-print("Các bộ nghiệm nguyên của phương trình 2x + 7y + 9z = 979 là:")
-for i  in nghiem :
-    print(i )
+for x in range(1, 980//2):  
+    for y in range(1, (980 - 2*x)//7 + 1):  
+        for z in range(1, (980 - 2*x - 7*y)//9 + 1):  
+            if 2*x + 7*y + 9*z == 979:
+                print(f"Bộ nghiệm: x = {x}, y = {y}, z = {z}")
+
