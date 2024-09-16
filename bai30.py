031da8e565c51897550041dd31b43b07cdde5d9f
NguyenThiCamNhung1402 <camnhung140261@gmail.com>
2024-09-16 12:08:56 +0700

    bai30.py

--- bai30.py
+++ bai30.py
@@ -4,27 +4,23 @@ Created on Thu Sep 12 22:26:42 2024
 
 @author: Nguyễn Thị Cẩm Nhung - 23712471 
 """
-ngay = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-while True:
-    thang = input("Nhập số tháng (1-12): ")
-    if thang.isdigit():
-        thang = int(thang)
-        if 1 <= thang <= 12:
-            break
+month = 0
+year = 0
+while month < 1 or month > 12:
+    month = int(input("Nhập tháng (1-12): "))
+    if month < 1 or month > 12:
+        print("Tháng không hợp lệ. Vui lòng nhập lại.")
+year = int(input("Nhập năm: "))
+nhuan = False
+if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+    nhuan = True
+if month in [1, 3, 5, 7, 8, 10, 12]:
+    days = 31 
+elif month in [4, 6, 9, 11]:
+    days = 30 
+elif month == 2:
+    if nhuan:
+        days = 29  
     else:
-        print("Nhập sai. Thử lại.")
-while True:
-    nam = input("Nhập số năm: ")
-    if nam.isdigit():
-        nam = int(nam)
-        break
-    else:
-        print("Nhập sai. Thử lại.")
-if thang == 2:
-    if (nam % 400 == 0 or (nam % 4 == 0 and nam % 100 != 0)):
-        print("Tháng này có 29 ngày")
-    else:
-        print("Tháng này có 28 ngày")
-else:
-    print(f"Tháng này có {ngay[thang - 1]} ngày")
-
+        days = 28  
+print(f"Tháng {month} năm {year} có {days} ngày.")
