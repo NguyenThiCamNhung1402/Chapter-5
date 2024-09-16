293ef7bef8ea81900416849ebf4ee187d8ec6910
NguyenThiCamNhung1402 <camnhung140261@gmail.com>
2024-09-16 11:11:52 +0700

    bai45.py

--- bai45.py
+++ bai45.py
@@ -7,9 +7,10 @@ Created on Thu Sep 12 23:15:59 2024
 x = float(input("Nhập giá trị x: "))
 n = int(input("Nhập số nguyên dương n: "))
 S = 0
+tong=0 
 while n <= 0   :
     n = int(input("Không hợp lệ. Nhập lại: "))
 for i in range(1, n + 1):
-   tong  = i * (i + 1) // 2
-   S += (x ** i) /tong 
+    tong+=i 
+    S += (x ** i) /tong 
 print("Tổng S là:", S)
