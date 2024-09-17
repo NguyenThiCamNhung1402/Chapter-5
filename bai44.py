d4a05ff0363382e7cf850f44cff65c819014a64f
NguyenThiCamNhung1402 <camnhung140261@gmail.com>
2024-09-17 19:15:55 +0700

    bai44.py

--- bai44.py
+++ bai44.py
@@ -9,5 +9,5 @@ S = 0
 while n <= 0   :
     n = int(input("Không hợp lệ. Nhập lại: "))
 for i in range(1, n + 1):
-    S += (2 * i + 1) / (2 * i + 1)
+    S += (2 * i + 1) / (2 * i + 2)
 print("Tổng S là:", S)
